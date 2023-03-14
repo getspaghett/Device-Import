4e8e83400b64959e31560aef34faf6ba661af066
Paul Christopher Schnürle <paul.schnuerle@rz.uni-freiburg.de>
2023-03-14 13:55:17 +0100

    copied nb-dt-import.py to keep existing framework for netbox connection and play around with my own code

---
+++ device-import.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+from collections import Counter
+from datetime import datetime
+import yaml
+import pynetbox
+from glob import glob
+import os
+
+import settings
+from netbox_api import NetBox
+
+from hw import HardwareInfo
+
+from device_type_template import DeviceType
+
+def find_vendor(dmi, vendors):
+    '''
+    Find the vendor from the dmi output in the list of vendors provided by the device-type library repo.
+    Returns true when a vendor matches the dmi output, false otherwise.
+    This function contains a list of possible matched vendors, not sure if this is useful yet.
+    TODO: Split into two functions
+    '''
+    dmi_vendor = dmi.manufacturer().lower()
+    match = False
+    possible_matches = []
+    for vendor in vendors:
+        if dmi_vendor in (vendor.get('name')).lower():
+            possible_matches.append(vendor.get('name'))
+            if dmi_vendor == (vendor.get('name')).lower():
+                print(f'Vendor found in DMI ({dmi_vendor}) matches vendor in Netbox ({vendor.get("name")})')
+                match = True
+                break
+    if match == False:
+        print(f'Vendor found in DMI ({dmi_vendor}) does not match any vendor in Netbox')
+        print(f'Possible matches: {possible_matches}')
+    return match
+
+def boolVendorMatch(dmi, vendors) -> bool:
+    '''
+    Returns true if the vendor listed in the dmi tables matches any vendor in the list of vendors provided by the device-type library repo.
+    '''
+    dmi_vendor = dmi.manufacturer().lower()
+    match = False
+    for vendor in vendors:
+        if dmi_vendor in (vendor.get('name')).lower():
+            match = True
+            break
+    return match
+
+def getMatchedVendor(dmi, vendors) -> str:
+    '''
+    Returns the vendor name that matches the vendor listed in the dmi tables.
+    '''
+    dmi_vendor = dmi.manufacturer().lower()
+    matchedVendor = ""
+    match = False
+    for vendor in vendors:
+        if dmi_vendor in (vendor.get('name')).lower():
+            match = True
+            matchedVendor = vendor.get('name')
+            break
+    if match == False:
+        print(f'Vendor found in DMI ({dmi_vendor}) does not match any vendor in Netbox')
+        matchedVendor = dmi_vendor
+    return matchedVendor
+
+def getPossibleMatches(dmi, vendors) -> list:
+    '''
+    Returns a list of possible matches for the vendor listed in the dmi tables.
+    '''
+    dmi_vendor = dmi.manufacturer().lower()
+    match = False
+    possible_matches = []
+    for vendor in vendors:
+        if dmi_vendor in (vendor.get('name')).lower():
+            possible_matches.append(vendor.get('name'))
+            if dmi_vendor == (vendor.get('name')).lower():
+                print(f'Vendor found in DMI ({dmi_vendor}) matches vendor in Netbox ({vendor.get("name")})')
+                match = True
+                break
+    if match == False:
+        print(f'Vendor found in DMI ({dmi_vendor}) does not match any vendor in Netbox')
+        print(f'Possible matches: {possible_matches}')
+    return possible_matches
+
+def main():
+    my_device = DeviceType()
+    startTime = datetime.now()
+    args = settings.args
+
+    # Init Hardware Info Class
+    dmi = HardwareInfo().getDmiInfo()# some of the pre-defined queries
+    print('Manufacturer:\t', dmi.manufacturer())
+    print('Model:\t\t', dmi.model())
+    print('Firmware:\t', dmi.firmware())
+    print('Serial number:\t', dmi.serial_number())
+    print('Processor type:\t', dmi.cpu_type())
+    print('Number of CPUs:\t', dmi.cpu_num())
+    print('Cores count:\t', dmi.total_enabled_cores())
+    print('Total RAM:\t{} GB'.format(dmi.total_ram()))
+    print('# DIMM Slots:\t', dmi.total_dimm_slots())
+    print('Chassis Type:\t', dmi.chassis_type())
+
+    netbox = NetBox(settings)
+    files, vendors = settings.dtl_repo.get_devices(
+        f'{settings.dtl_repo.repo_path}/device-types/', args.vendors)
+
+    settings.handle.log(f'{len(vendors)} Vendors Found')
+    # device_types = settings.dtl_repo.parse_files(files, slugs=args.slugs)
+    # settings.handle.log(f'{len(device_types)} Device-Types Found')
+    # Commented to not create vendors and device_types on Netbox yet.
+    # netbox.create_manufacturers(vendors)
+    # netbox.create_device_types(device_types)
+    print("Debug: -----")
+    match = boolVendorMatch(dmi, vendors)
+    print(f'Vendor Match: {match}')
+
+    with open('my_device.yaml', 'w') as file:
+        yaml.dump(my_device.getYAML(), file, sort_keys=False, explicit_start=True)
+    print('YAML file created')
+    if netbox.modules:
+        settings.handle.log("Modules Enabled. Creating Modules...")
+        files, vendors = settings.dtl_repo.get_devices(
+            f'{settings.dtl_repo.repo_path}/module-types/', args.vendors)
+        settings.handle.log(f'{len(vendors)} Module Vendors Found')
+        module_types = settings.dtl_repo.parse_files(files, slugs=args.slugs)
+        settings.handle.log(f'{len(module_types)} Module-Types Found')
+        # Commented to not create module_types yet.
+        # netbox.create_manufacturers(vendors)
+        # netbox.create_module_types(module_types)
+
+    settings.handle.log('---')
+    settings.handle.verbose_log(
+        f'Script took {(datetime.now() - startTime)} to run')
+    settings.handle.log(f'{netbox.counter["added"]} devices created')
+    settings.handle.log(
+        f'{netbox.counter["updated"]} interfaces/ports updated')
+    settings.handle.log(
+        f'{netbox.counter["manufacturer"]} manufacturers created')
+    if settings.NETBOX_FEATURES['modules']:
+        settings.handle.log(
+            f'{netbox.counter["module_added"]} modules created')
+        settings.handle.log(
+            f'{netbox.counter["module_port_added"]} module interface / ports created')
+
+
+if __name__ == "__main__":
+    main()
